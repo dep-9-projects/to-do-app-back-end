9239c859f30baccead4e50d05cbb7091cf355052
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2022-11-29 17:02:01 +0530

    update db-sql

--- src/main/java/lk/ijse/dep9/dao/custom/ToDoItemDAO.java
+++ src/main/java/lk/ijse/dep9/dao/custom/ToDoItemDAO.java
@@ -8,5 +8,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ToDoItemDAO extends CrudDAO<ToDoItem,Integer> {
+    void deleteAll();
 
 }
--- src/main/resources/db-script.sql
+++ src/main/resources/db-script.sql
@@ -12,3 +12,12 @@ CREATE TABLE `to-do-item`(
     status ENUM('DONE','NOT_DONE'),
     CONSTRAINT fk_name FOREIGN KEY (username) REFERENCES `user` (userName)
 );
+
+INSERT INTO user (userName, password, fullName) VALUES ('Manelka','123456789','Manelka Nishan'),
+                                                       ('Visal','2345678','Visal Srimal'),
+                                                       ('Pradeep','1234567','pradeep sampath'),
+                                                       ('Naveen','432562345','Naveen samaranayaka'),
+                                                       ('Chathura','836292702','Chathura Rupe');
+
+INSERT INTO `to-do-item` (username, description, status) VALUES ('Manelka','Go home gota','DONE'),
+                                                                ('Visal','Go Home Ranil','NOT_DONE');
