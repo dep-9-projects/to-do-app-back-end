71ae2dbd12f4750cb0dbf4eeb978aa2a08a34d5f
manelka <nishanmanelka@gmail.com>
2022-11-29 11:42:31 +0530

    add to-do-item table

--- src/main/resources/db-script.sql
+++ src/main/resources/db-script.sql
@@ -4,3 +4,11 @@ CREATE TABLE `user` (
     password VARCHAR(100) NOT NULL ,
     fullName VARCHAR (200) NOT NULL
 );
+
+CREATE TABLE `to-do-item`(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    username VARCHAR(100) NOT NULL ,
+    description VARCHAR(500) NOT NULL ,
+    status ENUM('DONE','NOT_DONE'),
+    CONSTRAINT fk_name FOREIGN KEY (username) REFERENCES `user` (userName)
+);
