18bf19e664611367a7efbba768c52d15de54d6e3
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2022-11-29 18:12:57 +0530

    test all DAO methods

--- src/main/resources/db-script.sql
+++ src/main/resources/db-script.sql
@@ -1,23 +1,25 @@
 
 CREATE TABLE `user` (
-    userName VARCHAR(100) PRIMARY KEY ,
-    password VARCHAR(100) NOT NULL ,
-    fullName VARCHAR (200) NOT NULL
+    `userName` VARCHAR(100) PRIMARY KEY ,
+    `password` VARCHAR(100) NOT NULL ,
+    `fullName` VARCHAR (200) NOT NULL
 );
 
 CREATE TABLE `to-do-item`(
-    id INT AUTO_INCREMENT PRIMARY KEY ,
-    username VARCHAR(100) NOT NULL ,
-    description VARCHAR(500) NOT NULL ,
-    status ENUM('DONE','NOT_DONE'),
-    CONSTRAINT fk_name FOREIGN KEY (username) REFERENCES `user` (userName)
+    `id` INT AUTO_INCREMENT PRIMARY KEY ,
+    `username` VARCHAR(100) NOT NULL ,
+    `description` VARCHAR(500) NOT NULL ,
+    `status` ENUM('DONE','NOT_DONE'),
+    CONSTRAINT fk_name FOREIGN KEY (`username`) REFERENCES `user` (`userName`)
 );
 
-INSERT INTO user (userName, password, fullName) VALUES ('Manelka','123456789','Manelka Nishan'),
+INSERT INTO `user` (`userName`, `password`, `fullName`) VALUES ('Manelka','123456789','Manelka Nishan'),
                                                        ('Visal','2345678','Visal Srimal'),
                                                        ('Pradeep','1234567','pradeep sampath'),
                                                        ('Naveen','432562345','Naveen samaranayaka'),
                                                        ('Chathura','836292702','Chathura Rupe');
 
-INSERT INTO `to-do-item` (username, description, status) VALUES ('Manelka','Go home gota','DONE'),
+INSERT INTO `to-do-item` (`username`, `description`, `status`) VALUES ('Manelka','Go home gota','DONE'),
                                                                 ('Visal','Go Home Ranil','NOT_DONE');
+
+
